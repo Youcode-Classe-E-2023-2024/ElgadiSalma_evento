cb88088d388c3746dd353a8a90da8ef91a91db19
Elgadisalma <elgadi.salma0@gmail.com>
2024-03-03 01:45:49 +0100

    Create dashboardController.php

---
+++ app/Http/Controllers/dashboardController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth; 
+
+class dashboardController extends Controller
+{
+    public function dashboardView()
+    {
+        // $userId = Auth::id();
+        // dd($userId);
+        return view('dashboard');
+    }
+}
