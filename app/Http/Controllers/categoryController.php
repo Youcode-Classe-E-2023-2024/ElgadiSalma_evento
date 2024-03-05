7a318af63fd59885099694e84d359a404e0d6099
Elgadisalma <elgadi.salma0@gmail.com>
2024-03-05 10:29:34 +0100

    add et display categories

--- app/Http/Controllers/categoryController.php
+++ app/Http/Controllers/categoryController.php
@@ -3,12 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Category;
 
 class categoryController extends Controller
 {
     public function categoryView()
     {
-        return view('Categories.category');
+        $categories = Category::all();
+        return view('Categories.category' , compact('categories'));
+    }
+
+    public function addCategory(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255|unique:categories,name'
+        ]);
+
+        Category::create([
+            'name' => $request->name
+        ]);
+
+        return redirect('/category');
     }
 
 }
