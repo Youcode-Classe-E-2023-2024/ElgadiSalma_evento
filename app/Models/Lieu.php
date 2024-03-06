be4c12ef4285546bb886a1ddc04669c761cf01f9
Elgadisalma <elgadi.salma0@gmail.com>
2024-03-06 12:51:56 +0100

    create model lieu

---
+++ app/Models/Lieu.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Lieu extends Model
+{
+    use HasFactory;
+    protected $table = 'lieu';
+
+    protected $fillable = ['ville'];
+}
